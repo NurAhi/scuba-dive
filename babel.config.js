cf9cf4a69e0842bb442379e15dd6731817c66ae1
Nur Ahissami <nur.ahissami@wefox.com>
2023-05-30 16:55:55 +0200

    chore: add config

---
+++ babel.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript', 'next/babel'],
+    plugins: [['styled-components', { ssr: true, displayName: true }]],
+    env: {
+      test: {
+        presets: [
+          [
+            '@babel/preset-env',
+            {
+              targets: 'current node',
+            },
+          ],
+        ],
+      },
+    },
+  };
+  
--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  pageExtensions: ['jsx', 'js', 'tsx', 'ts', 'scss', 'css'],
+  devIndicators: {
+    buildActivityPosition: 'bottom-right',
+  },
+  styledComponents: {
+    displayName: true,
+    ssr: true,
+  },
 }
 
 module.exports = nextConfig
